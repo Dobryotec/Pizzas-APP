a40cff606b8296d1ff83dcc722ca58649f7728a4
Dobryotec <dobrobatka@gmail.com>
2025-08-05 19:05:09 +0300

    update

--- pizza/src/main.tsx
+++ pizza/src/main.tsx
@@ -1,16 +1,16 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <BrowserRouter basename="/Pizzas-APP">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
